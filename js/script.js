33adfcd44e5ff5487ab542aeac42516cde96659c
Rafaelgeomo <65200578+Rafaelgeomo@users.noreply.github.com>
2023-07-28 19:05:43 -0300

    then, catch e finally

--- js/script.js
+++ js/script.js
@@ -1,3 +1,12 @@
-var consultaCEP = fetch('https://viacep.com.br/ws/01001000/json/');
+var consultaCEP = fetch ('https://viacep.com.br/ws/01001000/json/')
+.then(resposta => resposta.json())
+.then(r => {// then = então = quando a promessa é resolvida
+    if (r.erro){
+        throw Error ('Esse cep não existe!')
+    }else
+    console.log(r)
+}) 
+.catch(erro => console.log(erro)) //catch = pegue (no caso ele pegou o erro e imprimiu na tela) = quando a promessa é rejeitada
+.finally(mensagem => console.log('Processamento concluído!')); 
 
 console.log(consultaCEP);
