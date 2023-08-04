4b7963292d735f021984cbd7e52a769c38cb9c2a
Rafaelgeomo <65200578+Rafaelgeomo@users.noreply.github.com>
2023-08-04 15:54:19 -0300

    await e try

--- js/script.js
+++ js/script.js
@@ -1,12 +1,36 @@
-var consultaCEP = fetch ('https://viacep.com.br/ws/01001000/json/')
-.then(resposta => resposta.json())
-.then(r => {// then = então = quando a promessa é resolvida
-    if (r.erro){
-        throw Error ('Esse cep não existe!')
-    }else
-    console.log(r)
-}) 
-.catch(erro => console.log(erro)) //catch = pegue (no caso ele pegou o erro e imprimiu na tela) = quando a promessa é rejeitada
-.finally(mensagem => console.log('Processamento concluído!')); 
+async function buscaEndereco(cep){
+    try {
+    var consultaCEP = await fetch (`https://viacep.com.br/ws/${cep}/json/`);
+    var consultaCEPConvertida = await consultaCEP.json();
+    if (consultaCEPConvertida.erro){
+        throw Error('CEP não existente');
+    }
+    console.log(consultaCEPConvertida);
+    return consultaCEPConvertida;
+    } catch (erro) {
+        console.log(erro);
+    }
+}
 
-console.log(consultaCEP);
+buscaEndereco();
+
+
+
+// let ceps = ['01001000','01001001'];
+// let conjuntoCeps = ceps.map(valores => buscaEndereco(valores));// vai fazer um novo array com o que retornar da função buscaEndereco para cada um dos valores do de dentro do CEP, e esses valores vão ser promessas
+// console.log(conjuntoCeps);
+// Promise.all(conjuntoCeps).then(resposta => console.log(resposta)); //para resolver os arrays de promessas
+
+
+
+// .then(resposta => resposta.json())
+// .then(r => {// then = então = quando a promessa é resolvida
+//     if (r.erro){
+//         throw Error ('Esse cep não existe!')
+//     }else
+//     console.log(r)
+// }) 
+// .catch(erro => console.log(erro)) //catch = pegue (no caso ele pegou o erro e imprimiu na tela) = quando a promessa é rejeitada
+// .finally(mensagem => console.log('Processamento concluído!')); 
+
+
