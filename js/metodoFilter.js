0b97aa076f5e3638e7afe4bed269e91462525094
Rafaelgeomo <65200578+Rafaelgeomo@users.noreply.github.com>
2023-08-17 17:16:45 -0300

    filtrando os livros por classe e exibindo na tela

--- js/metodoFilter.js
+++ js/metodoFilter.js
@@ -1,8 +1,10 @@
-const btnFiltrarLivrosDeFront = document.getElementById('btnFiltrarLivrosFront');
-btnFiltrarLivrosDeFront.addEventListener('click',filtrarLivros)
+const botoes = document.querySelectorAll('.btn');
+botoes.forEach(btn => btn.addEventListener('click', filtrarLivros)); // forEach faz com que ele tenha um evento de click em todos os botoes de uma so vez chamando a function filtrarLivros
 
 function filtrarLivros(){
-    let livrosFiltrados = livros.filter(livros => livros.categoria == 'front-end')
-    console.table(livrosFiltrados)
+    const elementoBtn = document.getElementById(this.id) //pegar o id do botao que nos estamos (o elemento inteiro)
+    const categoria = elementoBtn.value //filtra somente o valor desse elemento
+    let livrosFiltrados = livros.filter(livros => livros.categoria == categoria)
+    exibirOsLivrosNaTela(livrosFiltrados)
 
 }
