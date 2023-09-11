3b61e00629728f0a3704bb949ebd2496db5d90ae
Rafaelgeomo <65200578+Rafaelgeomo@users.noreply.github.com>
2023-09-11 16:59:58 -0300

    Metodo reduce

--- js/metodoFilter.js
+++ js/metodoFilter.js
@@ -4,7 +4,27 @@ botoes.forEach(btn => btn.addEventListener('click', filtrarLivros)); // forEach 
 function filtrarLivros(){
     const elementoBtn = document.getElementById(this.id) //pegar o id do botao que nos estamos (o elemento inteiro)
     const categoria = elementoBtn.value //filtra somente o valor desse elemento
-    let livrosFiltrados = categoria == 'disponivel' ? livros.filter (livro => livro.quantidade > 0) : livros.filter (livro => livro.categoria == categoria)
+    let livrosFiltrados = categoria == 'disponivel' ? filtrarPorDisponibilidade() : filtrarPorCategoria(categoria)
     exibirOsLivrosNaTela(livrosFiltrados)
+    if(categoria == 'disponivel') {
+        const valorTotal = calcularValorTotalDeLivrosDisponiveis(livrosFiltrados)
+        exibirValorTotalDosLivrosDisponiveisNaTela(valorTotal)
+    }
 
+}
+
+function filtrarPorCategoria(categoria) {
+    return livros.filter(livro => livro.categoria == categoria);
+}
+
+function filtrarPorDisponibilidade() {
+    return livros.filter(livro => livro.quantidade > 0);
+}
+
+function exibirValorTotalDosLivrosDisponiveisNaTela(valorTotal){
+    elementoComValorTotalDeLivrosDisponiveis.innerHTML = `
+    <div class="livros__disponiveis">
+      <p>Todos os livros disponíveis por R$ <span id="valor">${valorTotal}</span></p>
+    </div>
+    `
 }
